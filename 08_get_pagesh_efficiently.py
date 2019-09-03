c6b60bd2c98e007eb6a74cc6d28e52d106cf1333
rusrom <rusrom@guyfawkes.33mail.com>
2019-09-03 18:28:29 +0300

    Efficient async page requests with one session

---
+++ 08_get_pagesh_efficiently.py
@@ -0,0 +1,33 @@
+import aiohttp
+import asyncio
+import time
+
+
+TARGET_URL = 'https://google.com/'
+# TARGET_URL = 'https://kartochki-domana.com.ua/ru/'
+# TARGET_URL = 'http://doman-cards.com.ua/'
+
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with session.get(url) as response:
+        print(f'Page took {time.time() - page_start}')
+        print(response.status)
+        return response.status
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+loop = asyncio.get_event_loop()
+urls = [TARGET_URL for i in range(50)]
+
+start = time.time()
+loop.run_until_complete(get_multiple_pages(loop, *urls))
+print(f'All took: {time.time() - start}')
